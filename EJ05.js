c328130d96a3f15aa995708d31777f89c3e5f8b4
Julieta-8 <48799592@est.ort.edu.ar>
2026-03-22 17:55:50 -0300

    ej5

--- EJ05.js
+++ EJ05.js
@@ -1,6 +1,16 @@
-import Producto from 'productos.js';
+import fs from 'fs';
 
-function buscarProducto(Obj){
-                 contenido = Producto.readFileSync(Obj, 'utf-8')
-    
+function buscarProducto(nombre) {
+    const contenido = fs.readFileSync('./productos.json', 'utf-8');
+    const productos = JSON.parse(contenido);
+
+    const producto = productos.find(p => p.nombre === nombre);
+
+    if (producto) {
+        console.log(producto);
+    } else {
+        console.log("Producto no encontrado");
+    }
 }
+
+buscarProducto("Mouse");
